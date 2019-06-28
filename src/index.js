3945a473f2908ec85709f22918e2000bd34faba3
KUDU <gavigan.ryan@gmail.com>
2019-06-28 13:41:00 -0400

    import icons

--- src/index.js
+++ src/index.js
@@ -17,28 +17,51 @@ import './index.css';
 //   ["http://splinternews.com", "./icons/splinternews.svg", "width: 74%"]
 // ];
 
-// const preload = {
-//   "data" : [ {
-//      "url": "https://reddit.com/r/ChapoTrapHouse",
-//       "icon": "icons/chapo.png",
-//       "style": "width: 42%;",
-//     },
-//     {"url": "https://discordapp.com/channels/@me", "icon": "icons/discord.png", "style": "width: 48%;"},
-//     {"url": "https://github.com", "icon": "./icons/github.png", "style": "width: 40%;"},
-//     {"url": "https://linkedin.com", "icon": "./icons/linkedin.png", "width: 70%; height: 65%;"},
-//     {"url": "https://youtube.com/feed/subscriptions", "icon": "./icons/youtube.png", "width: 45%;"},
-//     {"url": "https://soundcloud.com/stream",
-//       "icon": "./icons/soundcloud.png", "width: 50%;"},
-//     {"url": "https://play.google.com/music","./icons/google-play-music.png", "width: 40%;"},
-//     {"url": "https://patreon.com/home", "./icons/patreon.png", "width: 40%"},
-//     {"url": "https://getpocket.com", "./icons/pocket.png", "width: 40%;"},
-//     {"url": "http://theintercept.com","./icons/theintercept.svg", "width: 68%;"},
-//     {"url": "http://theverge.com","assets/icons/theverge.png", "width: 42%;"},
-//     {"url": "http://splinternews.com","assets/icons/splinternews.svg", "width: 74%;},
-//   ]
-// };
+//TODO: find better image import method
+import reddit from './icons/chapo.png';
+import discord from './icons/discord.png';
+import github from './icons/github.png';
+import linkedin from './icons/linkedin.png';
+import youtube from './icons/youtube.png';
+import playmusic from './icons/google-play-music.png';
+import soundcloud from './icons/soundcloud.png';
+import patreon from './icons/patreon.png';
+import pocket from './icons/.pocket.png';
+import theverge from './icons/thevergepng';
+import splinter from './icons/splinter.svg';
 
-import icon from './icons/github.png';
+const icons = [
+  reddit,
+  discord,
+  github,
+  linkedin,
+  youtube,
+  playmusic,
+  soundcloud,
+  patreon,
+  pocket,
+  theverge,
+  splinter,
+];
+
+class Item extends React.Component (test) {
+  constructor() {
+    super();
+    this.props.name = test.props.name;
+    this.props.icon = test.props.icon;
+  };
+  render() {
+    return (
+      <ul>
+        <li
+          href="https://github.com/"
+          img="./icons/github.png"
+          style={{width: '40%'}}>
+        </li>
+      </ul>
+    );
+  }
+}
 
 class Grid extends React.Component {
   render() {
@@ -55,20 +78,6 @@ class Grid extends React.Component {
     // });
   }
 }
-
-// class Item extends React.Component {
-//   render() {
-//     return (
-//       <ul>
-//         <li
-//           href="https://github.com/"
-//           img="./icons/github.png"
-//           style={{width: '40%'}}>
-//         </li>
-//       </ul>
-//     );
-//   }
-// }
 
 ReactDOM.render(
   <Grid />,
